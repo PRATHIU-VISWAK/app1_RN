f47e97b2714531f9f15ba45b1163c4a790e2f0bb
prathiuviswak <prathiuviswakdev@gmail.com>
2024-04-08 22:33:43 +0530

    first commit

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+
+
+export const Home = () => {
+    return (
+      <View style={styles.container}>
+        <Text>HOME</Text>
+      </View>
+    );
+  }
